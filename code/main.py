a29584b0e8ceb123f1d553dafaeb588e31c89d21
ebrahim <abrhymjwyd19@gmail.com>
2026-06-11 12:33:05 +0400

    me see screen me happy ayyy

--- code/main.py
+++ code/main.py
@@ -1,1 +1,26 @@
 from settings import *
+
+class Game:
+    def __init__(self):
+        pygame.init()
+        self.display_surface = pygame.display.set_mode((WINDOW_WIDTH, WINDOW_HEIGHT))
+        pygame.display.set_caption("awsome pokimoon")
+
+    def run (self):
+        while True:
+            #merow event loop
+            for event in pygame.event.get():
+                if event.type == pygame.QUIT:
+                    pygame.quit()
+                    exit()
+
+
+            # SHI GAME LOGIC
+            pygame.display.update() 
+if __name__ == '__main__':
+    game = Game()
+    game.run()
+
+
+
+
