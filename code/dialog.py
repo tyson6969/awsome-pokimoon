5511d4f7275a38df906c43be922d6daf13a08dfa
ebrahim <abrhymjwyd19@gmail.com>
2026-07-15 07:31:57 +0400

    dialog is mostly done

--- code/dialog.py
+++ code/dialog.py
@@ -1,16 +1,34 @@
 from settings import *
+from timer import Timer
 
 
 class DialogTree:
     def __init__(self, character, player, all_sprites, font):
         self.player = player
         self.character = character
-        self.font = font  # was 'font = font' — missing self!
+        self.font = font  
         self.all_sprites = all_sprites
         self.dialog = character.get_dialog()
         self.dialog_num = len(self.dialog)
         self.dialog_index = 0
         self.current_dialog = DialogSprite(self.dialog[self.dialog_index], self.character, self.all_sprites, self.font)
+        self.dialog_timer = Timer(500, autostart = True)
+
+    def input(self):
+        keys = pygame.key.get_just_pressed()
+        if keys[pygame.K_SPACE] and not self.dialog_timer.active:
+            self.current_dialog.kill()
+            self.dialog_index += 1
+            if self.dialog_index < self.dialog_num:
+                self.current_dialog = DialogSprite(self.dialog[self.dialog_index], self.character, self.all_sprites, self.font)
+                self.dialog_timer.activate()
+            else:
+                pass 
+
+
+    def update(self):
+        self.dialog_timer.update()
+        self.input()
 
 
 class DialogSprite(pygame.sprite.Sprite):
@@ -19,13 +37,14 @@ class DialogSprite(pygame.sprite.Sprite):
         self.z = WORLD_LAYERS['top']
 
         padding = 20
-        text_surf = font.render(message, False, COLORS['black'])  # use font, not self.font
+        text_surf = font.render(message, False, COLORS['black'])  
         padding = 5 
         width = max(30, text_surf.get_width() + padding *2 )
         height = text_surf.get_height() + padding *2 
 
-        surf = pygame.Surface((width, height))
-        surf.fill(COLORS['pure white'])
+        surf = pygame.Surface((width, height), pygame.SRCALPHA)
+        surf.fill((0,0,0,0)) # use this later 
+        pygame.draw.rect(surf, COLORS['pure white'], surf.get_frect(topleft = (0,0)), 0, 4)
         surf.blit(text_surf, text_surf.get_frect(center = (width /2 , height /2 )))
 
         self.image = surf
--- code/main.py
+++ code/main.py
@@ -28,6 +28,7 @@ class Game:
 
         self.import_assets()
         self.setup(self.tmx_maps['world'], 'house')
+        self.dialog_tree = None
 
     def import_assets(self):
         self.tmx_maps = {'world': load_pygame(join( 'data', 'maps', 'world.tmx')), 
@@ -93,16 +94,18 @@ class Game:
                         character_data = TRAINER_DATA[obj.properties['character_id']]) 
                 
     def input(self):
-        keys = pygame.key.get_just_pressed()
-        if keys[pygame.K_SPACE]:
-            for character in self.character_sprites: # yo chud ur going to use this later
-                if check_connections(100, self.player, character):
-                    self.player.block()
-                    character.change_facing_direction(self.player.rect.center)
-                    self.create_dialog(character)
+        if not self.dialog_tree:
+            keys = pygame.key.get_just_pressed()
+            if keys[pygame.K_SPACE]:
+                for character in self.character_sprites: # yo chud ur going to use this later
+                    if check_connections(100, self.player, character):
+                        self.player.block()
+                        character.change_facing_direction(self.player.rect.center)
+                        self.create_dialog(character)
         
     def create_dialog(self,character):
-        DialogTree(character, self.player, self.all_sprites, self.fonts['dialog'])
+        if not self.dialog_tree:
+            self.dialog_tree = DialogTree(character, self.player, self.all_sprites, self.fonts['dialog'])
         
 
     def run (self):
@@ -122,6 +125,7 @@ class Game:
             self.display_surface.fill("black")
             self.all_sprites.draw(self.player.rect.center)
 
+            if self.dialog_tree: self.dialog_tree.update()
 
             pygame.display.update()
             
