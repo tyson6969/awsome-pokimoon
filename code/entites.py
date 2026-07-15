3034f81db20e33002d26f9c45aa698dce8c93d2a
ebrahim <abrhymjwyd19@gmail.com>
2026-07-15 09:24:40 +0400

    better dialog

--- code/entites.py
+++ code/entites.py
@@ -1,4 +1,5 @@
 from settings import *
+from support import check_connections
 
 class Entity(pygame.sprite.Sprite):
     def __init__(self, pos, frames, groups, facing_direction):
@@ -52,16 +53,35 @@ class Entity(pygame.sprite.Sprite):
 
 
 class Character(Entity):
-    def __init__(self, pos, frames, groups, facing_direction, character_data):
+    def __init__(self, pos, frames, groups, facing_direction, character_data, player, create_dialog, collision_sprites, radius):
         super().__init__(pos, frames, groups, facing_direction)
         self.character_data = character_data
+        self.player = player
+        self.create_dialog = create_dialog
+        self.collision_rects = [sprite.rect for sprite in collision_sprites if sprite is not self]
+
+        self.has_moved = False
+        self.can_rotate = True
+        self.has_noticed = False
+        self.radius = int(radius)  
+        self.view_directions = character_data['directions']
 
     def get_dialog(self):
         return self.character_data['dialog'][f'{'defeated' if self.character_data['defeated'] else 'default'}']  # fixed
 
+    def raycast(self):
+        if check_connections(self.radius, self , self.player )and self.has_losd():
+            self.player.block()
+            self.player.change_facing_direction(self.rect.center)
+
+    def has_losd(self):
+        if vector(self.rect.center).distance_to(self.player.rect.center) < self.radius:
+            collisions = [bool(rect.clipline(self.rect.center , self.player.rect.center)) for rect in self.collision_rects]
+            return not any(collisions)
 
     def update(self, dt):
         self.animate(dt)
+        self.raycast()
 
 
 class Player(Entity):
